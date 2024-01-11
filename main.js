36ddbcd159cf08378acda725eb3abd2eeee30760
saikumarseepana <saikumarseepana436@gmail.com>
2024-01-11 11:52:00 +0530

    Adding commit message

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("hello world");
+let firstName = "Seepana ";
+let lastName = "Saikumar";
+let ans = 0;
+const gender = "NA"
+const personArray = ["sai", "kumar", "seepana"];
+if (gender == "Male") {
+    console.log("Hello Mr. "+firstName + lastName);
+} else if(gender == "Female"){
+    console.log("Hello Mrs. "+firstName + lastName);
+} else {
+    console.log("Hello "+firstName + lastName);
+}
+
+for(let i=0;i<=1000;i++){
+    ans = ans + i;
+    
+}
+console.log(ans);
+
